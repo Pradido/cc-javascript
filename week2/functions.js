74f635f35fbddd4f3c864c434ebc7c907133eaac
pradido <kidbrother50@yahoo.com>
2024-07-09 12:09:29 +0100

    functions

---
+++ week2/functions.js
@@ -0,0 +1,21 @@
+function sayHello(name){
+    return 'Hello,' + name;
+}
+console.log(sayHello('Tinubu'));
+console.log(sayHello('Atiku'));
+console.log(sayHello('Obi'));
+
+let x = 7;
+let y = 3;
+let z = x + y;
+console.log('Answer:' + z);
+
+for (let i = 1; i <= 10; i++ ) {
+    console.log('Hello world', + i );
+}
+
+
+for (let i = 1; i <= 15; i++){
+    console.log('Thank you', + i);
+}
+
