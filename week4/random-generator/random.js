fca7e62bfa5777e8c6ddd6c712d99b87f357ddeb
pradido <kidbrother50@yahoo.com>
2024-07-24 13:51:15 +0100

    Add a previous button to recall last quote

--- week4/random-generator/random.js
+++ week4/random-generator/random.js
@@ -1,20 +1,38 @@
 
-let quoteText = document.getElementsByTagName('h1')[0];
-let arthor = document.getElementsByTagName('h2')[0];
-let button = document.getElementsByTagName('button')[1];
+let quoteText = document.getElementsByTagName("h1")[0];
+let author = document.getElementsByTagName("h2")[0];
+let next = document.getElementsByTagName("button")[1];
+let previous = document.getElementsByTagName("button")[0];
+let savedQuote = '';
+window.onload = randomQuote();
 
 async function randomQuote(){
+    // Save the previous quote in local storage
+
+    sessionStorage.setItem('quote', quoteText.textContent);
+    sessionStorage.setItem('author', quoteText.textContent);
+
+    // Query the API for a new quote
     let url = 'https://api.quotable.io/random';
     let response = await fetch(url);
     let quoteContent = await response.json();
-
     // console.log(quoteContent.content);
     // console.log(quoteContent.arthur);
 
-    quoteText.innerHTML = quoteContent.content;
-    author.innerHTML = quoteContent.author;
+    // Write the new quote to HTML
+    quoteText.innerHTML = quoteContent.content
+    author.innerHTML = '- ' + quoteContent.author;
 }
 
-button.addEventListener('click', randomQuote);
+function getQuote(){
+    // write the previous quote to HTML
+    quoteText.innerHTML = sessionStorage.getItem('quote');
+    author.innerHTML = sessionStorage.getItem('author');
+};
+    
+
+// console.log(sessionStorage.getItem('quote'));
+next.addEventListener('click', randomQuote);
+previous.addEventListener('click', getQuote);
 
 // randomQuote();
